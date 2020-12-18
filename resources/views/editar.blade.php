ed87d4569b4f540830c1bc5ad9c3c7e42a5ee62b
Mateus Lecchi <mateus.lecchi.y@gmail.com>
2020-12-18 19:25:58 -0300

    Obrigação de preenchimento dos campos no formulário de edição

--- resources/views/editar.blade.php
+++ resources/views/editar.blade.php
@@ -5,21 +5,21 @@
         @csrf
         <div class="class my-3">
             <label class="form-label" for="">Nome Completo</label>
-            <input class="form-control" type="text" name="nome" value="{{ $usuario->nome  }}">
+            <input class="form-control" type="text" name="nome" value="{{ $usuario->nome  }}" required>
             <label class="form-label" for="">CPF</label>
-            <input class="form-control" type="text" name="cpf" value="{{ $usuario->cpf  }}"  pattern="\d{3}\.\d{3}\.\d{3}-\d{2}" title="Digite um CPF no formato: xxx.xxx.xxx-xx" >
+            <input class="form-control" type="text" name="cpf" value="{{ $usuario->cpf  }}"  pattern="\d{3}\.\d{3}\.\d{3}-\d{2}" title="Digite um CPF no formato: xxx.xxx.xxx-xx"  required>
             <label class="form-label" for="">Data de Nascimento</label>
-            <input class="form-control" type="date" name="nasc" value="{{ $usuario->nasc->format('Y-m-d')  }}">
+            <input class="form-control" type="date" name="nasc" value="{{ $usuario->nasc->format('Y-m-d')  }}" required>
             <label class="form-label" for="">Email</label>
-            <input class="form-control" type="email" name="email" value="{{ $usuario->email  }}">
+            <input class="form-control" type="email" name="email" value="{{ $usuario->email  }}" required>
             <label class="form-label" for="">Telefone</label>
-            <input class="form-control" type="text" name="telefone" value="{{ $usuario->telefone  }}">
+            <input class="form-control" type="text" name="telefone" value="{{ $usuario->telefone  }}" required>
             <label class="form-label" for="">Logradoudo</label>
-            <input class="form-control" type="text" name="logradouro" value="{{ $usuario->logradouro  }}">
+            <input class="form-control" type="text" name="logradouro" value="{{ $usuario->logradouro  }}" required>
             <label class="form-label" for="">Cidade</label>
-            <input class="form-control" type="text" name="cidade" value="{{ $usuario->cidade  }}">
+            <input class="form-control" type="text" name="cidade" value="{{ $usuario->cidade  }}" required>
             <label class="form-label" for="">Estado</label>
-            <input class="form-control" type="text" name="estado" value="{{ $usuario->estado  }}">
+            <input class="form-control" type="text" name="estado" value="{{ $usuario->estado  }}" required>
         </div>
         <button class="btn btn-primary">Salvar Alterações</button>
     </form>
