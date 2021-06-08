2d1e786ac433d20cd515753979470322498589d7
Mateus Lecchi <mlecchi@aracruz.es.gov.br>
2021-06-08 16:12:26 -0300

    Set Pagination on controller

--- app/Http/Controllers/UsuariosController.php
+++ app/Http/Controllers/UsuariosController.php
@@ -10,10 +10,10 @@ class UsuariosController extends Controller
 
     public function index() //funcao index para listar os usuarios cadastrados
     {
-        $usuario = Usuario::all('id','nome');
-        return view('listar',compact('usuario'));
+        $usuario = Usuario::paginate(10);
+        return view('listar', compact('usuario'));
     }
-    
+
 
     public function create() //funcao para chamar a view 'criar' para criacao de novo usuário no registro
     {
@@ -33,21 +33,21 @@ class UsuariosController extends Controller
             'estado' => $request->estado
         ]);
 
-        return redirect('')->with('success', 'Usuario cadastrado com sucesso.'); //retorno da mensagem de sucesso
+        return redirect('usuarios')->with('success', 'Usuario cadastrado com sucesso.'); //retorno da mensagem de sucesso
     }
 
     public function show($id) //funcao para mostrar destalhes do usuario individual
     {
         $usuario = Usuario::findOrFail($id);
-        return view('usuario',['usuario' => $usuario]);
+        return view('usuario', ['usuario' => $usuario]);
     }
-    
 
-    
+
+
     public function edit($id) //funcao para editar dados do usuario
     {
         $usuario = Usuario::findOrFail($id);
-        return view('editar',['usuario' => $usuario]);
+        return view('editar', ['usuario' => $usuario]);
     }
 
     public function update(Request $request, $id) //funcao para atualizar edicoes no banco
@@ -65,13 +65,13 @@ class UsuariosController extends Controller
             'estado' => $request->estado
         ]);
 
-        return redirect('')->with('success', 'Usuario alterado com sucesso.'); //retorna mensagem de sucesso na alteração dos dados
+        return redirect('usuarios')->with('success', 'Usuario alterado com sucesso.'); //retorna mensagem de sucesso na alteração dos dados
     }
 
     public function delete($id) //funcao para deletar registro
     {
         $usuario = Usuario::findOrFail($id);
-        return view('excluir',['usuario' => $usuario]);
+        return view('excluir', ['usuario' => $usuario]);
     }
 
     public function destroy($id) //funcao para executar o delete no banco
@@ -79,6 +79,6 @@ class UsuariosController extends Controller
         $usuario = Usuario::findOrFail($id);
         $usuario->delete();
 
-        return redirect('')->with('success', 'Usuario excluído com sucesso.'); //retorna mensagem de sucesso ao deletar usuario
+        return redirect('usuarios')->with('success', 'Usuario excluído com sucesso.'); //retorna mensagem de sucesso ao deletar usuario
     }
 }
